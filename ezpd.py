928e4280f73aacef307fe628a80f567cfbc6e741
JacksonTeh <jacksenteh@gmail.com>
2020-04-03 13:36:26 +0800

    slight modification on the pretty print

--- ezpd.py
+++ ezpd.py
@@ -11,13 +11,13 @@ def checkDfInfo(df):
     print('Number of rows   : ', nb_rows)
     print('Number of columns: ', len(columns))
     
-    print('=' * 98)
-    print('{:3}  | {:40} | {:15} | {:30}'.format('Idx', 'Name', 'dtype', 'Nb of Missing Values'))
-    print('=' * 98)
+    print('=' * 93)
+    print('{:3}  | {:40} | {:15} | {:25}'.format('Idx', 'Name', 'dtype', 'Nb of Missing Values (%)'))
+    print('=' * 93)
     
     for i, col in enumerate(columns):
         nb_missing = df[col].isna().sum()
         pr_missing = nb_missing / nb_rows * 100
-        print('{:3}) | {:40} | {:15} | {:20} ({:6.2f}%)'.format(i, col, str(df[col].dtype), nb_missing, pr_missing))
+        print('{:3}) | {:40} | {:15} | {:15} ({:6.2f}%)'.format(i, col, str(df[col].dtype), nb_missing, pr_missing))
     
-    print('=' * 98)
+    print('=' * 93)
