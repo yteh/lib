a53ceebe8391567bad35f11938a1e8c61ebd381c
JacksonTeh <jacksenteh@gmail.com>
2020-04-24 19:33:56 +0800

    fixed sns bug @ preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -7,6 +7,7 @@ TODO:
 import pandas            as pd
 import numpy             as np
 import matplotlib.pyplot as plt
+import seaborn           as sns
 import json
 
 from sklearn.preprocessing import LabelEncoder
